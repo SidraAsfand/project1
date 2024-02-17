27aeaa2a96d458d11445a21f3e28a9a30725a92e
SidraAsfand <159244342+SidraAsfand@users.noreply.github.com>
2024-02-17 14:25:31 -0800

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,44 @@
+import inquirer  from  "inquirer";
+
+const answers :{
+    "NO1" :number;
+    "NO2" :number;
+    "operator" :string;
+} = await inquirer.prompt([{
+    type: "number",
+    name: "NO1",
+    message: "write your No1: "
+
+},
+{
+    type: "number",
+    name: "NO2",
+    message: "write your No2: "
+
+},
+{
+type: "list",
+    name: "operator",
+    message: "Select your  operator  ",
+    choices:["+","-","*","/"]
+
+}
+])
+const {NO1,NO2,operator} =  answers
+if (NO1 && NO2 && operator){
+    let result :number =0;
+    if(operator==="+"){
+        result = NO1 +  NO2
+    }
+    else if(operator=== "-"){
+        result = NO1 -  NO2
+    }
+   else if(operator==="*"){
+        result = NO1 *  NO2
+    }
+   else if(operator==="/"){
+        result = NO1 /  NO2
+    }console.log("YOUR result is:" + result)
+}else{
+    console.log("please select a valid operator")
+}
